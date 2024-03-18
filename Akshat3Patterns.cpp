56c0df21bdc109ed201ec5f431ff348be88c823a
Akshat Khandelwal <akshat.khandelwal2002@gmail.com>
2024-03-18 21:00:53 +0530

    Patterns

---
+++ Akshat3Patterns.cpp
@@ -0,0 +1,964 @@
+                                //Patterns
+             //Square patterns
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter the number"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<"*";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<"*";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the no."<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<j;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+4444
+4444
+4444*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the no."<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<"n";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout << "Enter the value" << endl;
+//     cin >> n;
+//     int i = 1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<i;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter the value"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<j;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the no."<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){                
+//         int j=n;                     
+//         while(j>0){                  
+//             cout<<j;                 
+//             j--;                     
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter the value"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<n-j+1;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+ 
+
+        //Triangular Patterns
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the value"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<j;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the value"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=i;
+//         while(j<=2*i-1){
+//             cout<<j;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the value"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int k=i;
+//         int j=1;
+//         while(j<=i){
+//             cout<<k;
+//             j++;
+//             k++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     int j=1;
+//     while(i<=n){
+//         int k=1;
+//         while(k<=i){
+//             cout<<j;
+//             k++;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+*
+**
+***
+****
+Note : There are no spaces between the stars (*).*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the value"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<"*";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+22
+333
+4444*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter the value"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<i;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+21
+321
+4321*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the no."<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=i;
+//         while(j>0){
+//             cout<<j;
+//             j--;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+      
+         //Character Patterns
+         
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the no."<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             char ch='A'+j-1;
+//             cout<<ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the no"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         char startChar='A'+i-1;
+//         int j=1;
+//         while(j<=n){
+//             char ch=startChar+j-1;
+//             cout<<ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 3
+A
+BB
+CCC*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the no"<<endl;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         char Ch='A'+i-1;
+//         int j=1;
+//         while(j<=i){
+//             cout<<Ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+A
+BC
+CDE
+DEFG*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         char startCh='A'+i-1;
+//         int j=1;
+//         while(j<=i){
+//             char ch=startCh+j-1;
+//             cout<<ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+/*Print the following pattern for the given number of rows.
+Pattern for N = 5
+E
+DE
+CDE
+BCDE
+ABCDE*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         char startCh='A'+n-i;
+//         int j=1;
+//         while(j<=i){
+//             char ch=startCh+j-1;
+//             cout<<ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+        // Reverse Triangle Pattern
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n-i){
+//             cout<<" ";
+//             j++;
+//         }
+//         int k=1;
+//         while(k<=i){
+//             cout<<"*";
+//             k++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+...1
+..12
+.123
+1234
+The dots represent spaces.*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n-i){
+//             cout<<" ";
+//             j++;
+//         }
+//         int k=1;
+//         while(k<=i){
+//             cout<<k;
+//             k++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n-i+1){
+//             cout<<"*";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n-i+1){
+//             cout<<j;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+
+/* Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+333
+22
+1          */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n-i+1){
+//             cout<<n-i+1;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n-i){
+//             cout<<" ";
+//             j++;
+//         }
+//         int k=1;
+//         while(k<=i){
+//             cout<<k;
+//             k++;
+//         }
+//         int l=i-1;
+//         while(l>0){
+//             cout<<l;
+//             l--;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+/* Print the following pattern
+Pattern for N = 4
+...*
+..***
+.*****
+*******
+The dots represent spaces. */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n){
+//                 int j=1;
+//                 while(j<=n-i){
+//                         cout<<" ";
+//                         j++;
+//                 }
+//                 int k=1;
+//                 while(k<=2*i-1){
+//                         cout<<"*";
+//                         k++;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+// }
+
+
+/* Print the following pattern for the given number of rows.
+Pattern for N = 4
+...1
+..232
+.34543
+4567543
+The dots represent spaces. */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n){
+//                 int j=1;
+//                 while(j<=n-i){
+//                         cout<<" ";
+//                         j++;
+//                 }
+//                 int k=1;
+//                 int l=i;
+//                 while(k<=i){
+//                         cout<<l;
+//                         l++;
+//                         k++;
+//                 }
+//                 int m=2*i-2;
+//                 while(m>=i){
+//                         cout<<m;
+//                         m--;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+// }
+
+
+
+/* Print the following pattern for the given number of rows.
+Note: N is always odd.
+Pattern for N = 5
+..*
+.***
+*****
+.***
+..*
+The dots represent spaces. */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n/2+1){
+//                 int j=1;
+//                 while(j<=n/2+1-i){
+//                         cout<<" ";
+//                         j++;
+//                 }
+//                 int k=1;
+//                 while(k<=2*i-1){
+//                         cout<<"*";
+//                         k++;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+//         int l=1;
+//         while(l<=n/2){
+//                 int m=1;
+//                 while(m<=l){
+//                         cout<<" ";
+//                         m++;
+//                 }
+//                 int p=2*(n/2-l+1)-1;
+//                 while(p>0){
+//                         cout<<"*";
+//                         p--;
+//                 }
+//                 cout<<endl;
+//                 l++;
+//         }
+// }
+
+
+/*  Print the following pattern:
+55555
+45555
+34555
+23455
+12345  */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n){
+//                 int j=1;
+//                 int k=n-i+1;
+//                 while(j<=i){
+//                         cout<<k;
+//                         k++;
+//                         j++;
+//                 }
+//                 int l=1;
+//                 while(l<=n-i){
+//                         cout<<n;
+//                         l++;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+// }
+
+
+/*  Print the following pattern:
+ABCDE
+ABCD
+ABC
+AB
+A      */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n){
+//                 int j=1;
+//                 while(j<=n-i+1){
+//                         char ch='A'+j-1;
+//                         cout<<ch;
+//                         j++;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+// }
+
+
+/*  Print the following pattern:
+12344321
+123**321
+12****21
+1******1   */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n){
+//                 int j=1;
+//                 while(j<=n-i+1){
+//                         cout<<j;
+//                         j++;
+//                 }
+//                 int k=1;
+//                 while(k<=2*i-2){
+//                         cout<<"*";
+//                         k++;
+//                 }
+//                 int l=n-i+1;
+//                 while(l>0){
+//                         cout<<l;
+//                         l--;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+// }
+
+
+/*  Print the following pattern:
+....1
+...123
+..12345
+.1234567
+123456789
+.1234567
+..12345
+...123
+....1         */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n/2+1){
+//                 int j=1;
+//                 while(j<=n/2-i+1){
+//                         cout<<" ";
+//                         j++;
+//                 }
+//                 int k=1;
+//                 while(k<=2*i-1){
+//                         cout<<k;
+//                         k++;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+//         int l=1;
+//         while(l<=n/2){
+//                 int m=1;
+//                 while(m<=l){
+//                         cout<<" ";
+//                         m++;
+//                 }
+//                 int p=1;
+//                 while(p<=2*(n/2-l+1)-1){
+//                         cout<<p;
+//                         p++;
+//                 }
+//                 cout<<endl;
+//                 l++;
+//         }
+// }
+
+
+
+/*  Print the following patern:
+1.....1
+.2...2
+..3.3
+...4
+..3.3
+.2...2
+1.....1  */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//         int n;
+//         cin>>n;
+//         int i=1;
+//         while(i<=n/2+1){
+//                 int j=1;
+//                 while(j<=i-1){
+//                         cout<<" ";
+//                         j++;
+//                 }
+//                 int k=1;
+//                 while(k<=1){
+//                         cout<<i;
+//                         k++;
+//                 }
+//                 int l=n/2-2*i+3;
+//                 while(l>0){
+//                         cout<<" ";
+//                         l--;
+//                 }
+//                 int m=1;
+//                 while(m<=1){
+//                         cout<<i;
+//                         m++;
+//                 }
+//                 cout<<endl;
+//                 i++;
+//         }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int i, j, k;
+//   for (i = 1; i <= 7; i++) {
+//     if (i <= 4) {
+//       for (j = 1; j <= 4 - i; j++) {
+//         cout << " ";
+//       }
+//       cout << i;
+//       for (k = 1; k <= 2 * (i - 1) - 1; k++) {
+//         cout << " ";
+//       }
+//       cout << i;
+//     } else {
+//       for (j = 1; j <= i - 4; j++) {
+//         cout << " ";
+//       }
+//       cout << 8 - i;
+//       for (k = 1; k <= 2 * (7 - i) - 3; k++) {
+//         cout << " ";
+//       }
+//       cout << 8 - i;
+//     }
+//     cout << endl;
+//   }
+//   return 0;
+// }
